e5d20a931b7cf7396c8a3fb11a424cb7dfe1a05b
Shim <Shim@DESKTOP-00N04SV>
2023-06-29 21:25:00 +0800

    models schemas

---
+++ models/travelmodel.js
@@ -0,0 +1,26 @@
+const mongoose =require('mongoose')
+
+const Schema = mongoose.Schema
+
+const travelSchema = new Schema(
+  {
+    place: {
+      type: String,
+      require: true,
+    },
+    from: {
+      type: String,
+      require: true,
+    },
+    to: {
+      type: String,
+      require: true,
+    },
+    experience: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model('Travel',travelSchema)
--- routes/travel.js
+++ routes/travel.js
@@ -1,22 +1,33 @@
 const express = require("express")
-
+const Travel = require('../models/travelmodel')
 const router = express.Router()
 
 //Get all
 router.get('/',(req,res) => {
     res.json({mssg:'Get all the Travel Info'})
 })
+
 //Get single
 router.get('/:id',(req,res)=>{
     res.json({mssg:'Get specific Travel'})
 })
+
 //POST
-router.post('/',(req,res) =>{
-    res.json({mssg:'Post a Travel'})
+router.post('/',async (req,res) =>{
+    const {place,from,to,experience} = req.body
+
+    try{
+      const travel = await Travel.create({ place, from, to, experience });
+      res.status(200).json(travel)
+      } catch(error){
+      res.status(400).json({error: error.message});
+    }
 })
+
 router.delete("/", (req, res) => {
   res.json({ mssg: "Delete a Travel" });
 });
+
 router.patch("/", (req, res) => {
   res.json({ mssg: "Update a Travel" });
 });
