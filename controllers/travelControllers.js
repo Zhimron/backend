15bc580118a0d2a9692033cbc661ec595415674f
Zhimron <gurayshimron@gmail.com>
2023-07-06 11:07:12 +0800

    cors

--- controllers/travelControllers.js
+++ controllers/travelControllers.js
@@ -14,6 +14,7 @@ const getTravel = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No Travel Id" });
   }
+
   const travel = await Travel.findById(id);
 
   if (!travel) {
--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-require('dotenv').config()
+require("dotenv").config();
+const cors = require("cors");
 
-const mongoose = require('mongoose')
-const express = require('express')
-const travelRoutes = require('./routes/travel')
-//express 
+const mongoose = require("mongoose");
+const express = require("express");
+const travelRoutes = require("./routes/travel");
+//express
+const myapp = express();
+myapp.use(cors());
 
-const myapp = express()
+myapp.use(express.json());
 
-myapp.use(express.json())
-
-myapp.use((req,res,next) => {
-    console.log(req.path, req.method)
-    next()
-})
+myapp.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes for travel
-myapp.use('/api/travel',travelRoutes)
+myapp.use("/api/travel", travelRoutes);
 
 //connect database
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        //request
-        myapp.listen(process.env.PORT, () => {
-          console.log("You look Lonely", process.env.PORT);
-        });
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
-
-
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //request
+    myapp.listen(process.env.PORT, () => {
+      console.log("You look Lonely", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
